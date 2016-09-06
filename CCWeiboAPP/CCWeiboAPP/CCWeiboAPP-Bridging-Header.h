4dabb6b37c9ef525da92e647c6a5380f17752aa7
chester39 <chester39@163.com>
2016-09-06 22:13:35 +0800

    add TZImagePickerController

--- CCWeiboAPP/CCWeiboAPP/CCWeiboAPP-Bridging-Header.h
+++ CCWeiboAPP/CCWeiboAPP/CCWeiboAPP-Bridging-Header.h
@@ -10,5 +10,6 @@
 #import <MBProgressHUD/MBProgressHUD.h>
 #import <SDWebImage/UIImageView+WebCache.h>
 #import <MJRefresh/MJRefresh.h>
+#import <TZImagePickerController/TZImagePickerController.h>
 
 #endif /* CCWeiboAPP_Bridging_Header_h */
--- CCWeiboAPP/Pods/Target Support Files/Pods-CCWeiboAPP/Pods-CCWeiboAPP-frameworks.sh
+++ CCWeiboAPP/Pods/Target Support Files/Pods-CCWeiboAPP/Pods-CCWeiboAPP-frameworks.sh
@@ -90,6 +90,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/TZImagePickerController/TZImagePickerController.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
@@ -98,4 +99,5 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/TZImagePickerController/TZImagePickerController.framework"
 fi
